1407a14ad484ef22eede0b2311e7350ff95c9e81
xept90 <xept90@fdf4652a-fd1b-2c7c-1bbe-f5f4c912a413>
2010-12-31 00:12:11 +0000

    Syntax highlighting added

---
+++ highlighter.h
@@ -0,0 +1,44 @@
+#ifndef HIGHLIGHTER_H
+#define HIGHLIGHTER_H
+
+#include "editor.h"
+#include <QSyntaxHighlighter>
+#include <QTextDocument>
+#include <QTextEdit>
+#include <QString>
+#include <QVector>
+#include <QTextFormat>
+#include <QFont>
+
+class Editor;
+
+class Highlighter : public QSyntaxHighlighter
+{
+private:
+    // Q_OBJECT macro
+    Q_OBJECT
+    // Highlightingrule Struct
+    struct HighlightingRule
+    {
+	QRegExp word,start,end;
+	QTextCharFormat format;
+    };
+    // Vector of highlightingrules
+    QVector<HighlightingRule> multiRules;
+    QVector<HighlightingRule> singleRules;
+    // Pointer to editor
+    Editor * editor;
+    // Helper method
+    void findFormatHelper(QString& data,HighlightingRule& rule,int i);
+protected:
+    void highlightBlock(const QString &text);
+public:
+    // Constructor
+    Highlighter(Editor * editor, QTextDocument *parent = 0);
+    // Methods
+    void loadSyntax(QString filename);
+    void selectSyntax();
+    QString translate(QString str);
+};
+
+#endif // HIGHLIGHTER_H
